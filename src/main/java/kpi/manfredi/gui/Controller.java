bb9f7e01a18c6375ec85999f117b894809b2d6d7
MrManfredi <mrmanfredi228@gmail.com>
2019-08-02 22:39:25 +0300

    Added a preview image by clicking on an item from the list and possibility to open image in the default image viewer.

--- src/main/java/kpi/manfredi/gui/Controller.java
+++ src/main/java/kpi/manfredi/gui/Controller.java
@@ -8,12 +8,21 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.awt.*;
 import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -41,13 +50,56 @@ public class Controller {
     @FXML
     private MenuItem about_menuItem;
 
+    @FXML
+    private ImageView preview_image;
+
+    @FXML
+    private Button open_image_button;
+
     public void initialize() {
         images_list_view.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        initMy();
         handle_add_button();
         handle_remove_selected_button();
         handle_remove_duplicates_button();
         handle_delete_selected_button();
         handle_about_menuItem();
+        handle_open_image_button();
+    }
+
+    private void initMy() {
+        images_list_view.setCellFactory(lv -> {
+            ListCell<File> cell = new ListCell<>() {
+
+                @Override
+                protected void updateItem(File item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (item != null)
+                        setText(item.getName());
+                    else
+                        setText("");
+                }
+
+            };
+
+            cell.setOnMouseClicked(e -> {
+                if (!cell.isEmpty()) {
+                    logger.info("You clicked on " + cell.getItem());
+                    handle_click_on_image_in_list(cell.getItem());
+//                    e.consume();
+                }
+            });
+            return cell;
+        });
+    }
+
+    private void handle_click_on_image_in_list(File file) {
+        try {
+            Image image = new Image(file.toURI().toURL().toString());
+            preview_image.setImage(image);
+        } catch (MalformedURLException ex) {
+            logger.error(ex.getMessage());
+        }
     }
 
     private void handle_add_button() {
@@ -83,7 +135,7 @@ public class Controller {
             }
             return true;
         } catch (Exception ex) {
-            logger.error(ex.getMessage());
+            logger.warn(ex.getMessage());
             return false;
         }
     }
@@ -97,6 +149,7 @@ public class Controller {
         remove_selected_button.setOnMouseClicked(event -> {
             ObservableList<File> filesToRemove = images_list_view.getSelectionModel().getSelectedItems();
             images_list_view.getItems().removeAll(filesToRemove);
+            setDefaultIfImageNotExists();
         });
     }
 
@@ -109,6 +162,7 @@ public class Controller {
         List<File> filesList = distinct.collect(Collectors.toList());
         ObservableList<File> filesObservableList = FXCollections.observableArrayList(filesList);
         images_list_view.setItems(filesObservableList);
+        setDefaultIfImageNotExists();
     }
 
     private void handle_delete_selected_button() {
@@ -124,6 +178,7 @@ public class Controller {
             if (result.isPresent() && result.get() == ButtonType.OK) {
                 logger.info("Pressed on delete button. Result is OK! {} files to be deleted. ", filesToDelete.size());
                 deleteFiles(filesToDelete);
+                setDefaultIfImageNotExists();
             } else {
                 logger.info("Pressed on delete button. Result is CANCEL!");
             }
@@ -173,5 +228,43 @@ public class Controller {
                     "image processing (renaming, deleting, etc.).");
             alert.showAndWait();
         });
+    }
+
+    private void setDefaultIfImageNotExists() {
+        if (preview_image != null) {
+            File file = convertToFile(preview_image.getImage());
+            if (file != null && !file.exists()) {
+                logger.info("Set to default because the following image does not exist: {}", file);
+                Image image = new Image("preview_image.jpg");
+                preview_image.setImage(image);
+            }
+        }
+    }
+
+    private void handle_open_image_button() {
+        open_image_button.setOnMouseClicked(event -> {
+            if (preview_image != null) {
+                File file = convertToFile(preview_image.getImage());
+                if (file != null) {
+                    Desktop desktop = Desktop.getDesktop();
+                    try {
+                        desktop.open(file);
+                    } catch (IOException e) {
+                        logger.error(e.getMessage());
+                    }
+                }
+            }
+        });
+    }
+
+    private File convertToFile(Image image) {
+        try {
+            URL url = new URL(image.getUrl());
+            URI uri = url.toURI();
+            return new File(uri);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return null;
+        }
     }
 }
