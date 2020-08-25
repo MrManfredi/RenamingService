098f7de609b57a7a684948623d94ffa408e1928c
Manfredi <mrmanfredi228@gmail.com>
2020-08-25 19:44:07 +0300

    Monitoring service initiated. Parameterized launch added.

--- src/main/java/kpi/manfredi/MainLoader.java
+++ src/main/java/kpi/manfredi/MainLoader.java
@@ -2,9 +2,38 @@ package kpi.manfredi;
 
 import javafx.application.Application;
 import kpi.manfredi.gui.JavaFxMain;
+import kpi.manfredi.monitoring.MonitoringService;
 
 public class MainLoader {
     public static void main(String[] args) {
-        Application.launch(JavaFxMain.class, args);
+        if (args.length == 0) {
+            Application.launch(JavaFxMain.class, args);
+        } else if (args.length == 1) {
+            switch (args[0]) {
+                case "-h":
+                    showHelp();
+                    break;
+                case "-m":
+                    new MonitoringService().run();
+                    break;
+                default:
+                    showError();
+            }
+        } else {
+            showError();
+        }
+
+    }
+
+    private static void showError() {
+        System.err.println("Wrong list of arguments.");
+        showHelp();
+    }
+
+    private static void showHelp() {
+        System.out.println("Usage: java -jar <application-name> [-h | -m]");
+        System.out.println("without param. - gui application");
+        System.out.println("-h  - this help information");
+        System.out.println("-m  - directory monitoring service");
     }
 }
---
+++ src/main/java/kpi/manfredi/monitoring/MonitoringService.java
@@ -0,0 +1,9 @@
+package kpi.manfredi.monitoring;
+
+public class MonitoringService implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println("Run service");
+    }
+}
